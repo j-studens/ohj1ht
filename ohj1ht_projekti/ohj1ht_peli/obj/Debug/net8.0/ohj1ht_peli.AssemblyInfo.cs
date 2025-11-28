f8f9de6afa4a565115369746b85635cb6c05b399
Professional Git <244223496+j-studens@users.noreply.github.com>
2025-11-28 20:40:44 +0200

    1.2 Hiontaa
    - Aliohjelmat & attribuutin private
    - Attribuutit taulukkoon
    - Koodikäytänteiden varmistaminen
    - Virheilmoitusten korjaaminen
    - Dokumentaatio

--- ohj1ht_projekti/ohj1ht_peli/obj/Debug/net8.0/ohj1ht_peli.AssemblyInfo.cs
+++ ohj1ht_projekti/ohj1ht_peli/obj/Debug/net8.0/ohj1ht_peli.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ohj1ht_peli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04d06f447acf2c699e910b1be32a0b2f352bfafd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49bcfad5b47bdcd19767d86475d840cfa6efa899")]
 [assembly: System.Reflection.AssemblyProductAttribute("ohj1ht_peli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ohj1ht_peli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ohj1ht_projekti/ohj1ht_peli/ohj1ht_peli.cs
+++ ohj1ht_projekti/ohj1ht_peli/ohj1ht_peli.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
 using Jypeli;
 using Jypeli.Assets;
-using Jypeli.Controls;
-using Jypeli.Widgets;
 
 /// @author J
 /// @version 19.11.2025
@@ -12,26 +9,35 @@ using Jypeli.Widgets;
 /// </summary>
 public class ohj1ht_peli : PhysicsGame
 {
-    int kenttaNro = 1;
-    private const int pisteraja1 = 4;
-    private const int pisteraja2 = 7;
-    private const int pisteraja3 = 9;
-    private const int pisteraja4 = 11;
-    private const int pisteraja5 = 13;
-    private const int pisteraja6 = 15;
+    
+    // Piste-attribuutit.    
+    private const int PisteRaja1 = 4;
+    private const int PisteRaja2 = 7;
+    private const int PisteRaja3 = 9;
+    private const int PisteRaja4 = 11;
+    private const int PisteRaja5 = 13;
+    private const int PisteRaja6 = 15;
+    private IntMeter _pelaajanPisteet;
+    
+    // Kenttä attribuutti.
+    private int _kenntaNro = 1;
+    
+    // Fysiikka-attribuutit.
+    private const double Nopeus = 200;
+    private const double HyppyNopeus = 750;
+    private const int RuudunKoko = 40;
+    
+    // Kenttätaulukko.
+    private static readonly string[] KenttaTaulukko ={"kentta1.txt","kentta2.txt","kentta3.txt"};
+    
+    // Kuvataulukko. 
+    private Image[] _kuvaTaulukko = {LoadImage("ilkeä_ukko.png"),LoadImage("jere_default.png"),LoadImage("lahja.png")};
     
     
-    IntMeter pelaajanPisteet;
-    private const double NOPEUS = 200;
-    private const double HYPPYNOPEUS = 750;
-    private const int RUUDUN_KOKO = 40;
-
-    private Image VihollinenKuva = LoadImage("ilkeä_ukko.png");
-    private Image pelaajanKuva = LoadImage("jere_default.png");
-    private Image tahtiKuva = LoadImage("lahja.png");
-    private PlatformCharacter pelaaja1;
-    private PlatformCharacter vihollinen;
-    private PhysicsObject nyrkki;
+    // Olio-attribuutit
+    private PlatformCharacter _pelaaja1;
+    private PlatformCharacter _vihollinen;
+    private PhysicsObject _nyrkki;
 
 
     public override void Begin()
@@ -41,36 +47,92 @@ public class ohj1ht_peli : PhysicsGame
 
     }
 
-    void LuoKentta(string seuraavakentta)
+    
+    /// <summary>
+    /// Kategoria: PELIKENTTÄ | Aliohejlma, joka luo varsinaisen pelikentän.
+    /// </summary>
+    /// <param name="seuraavakentta"></param>
+    private void LuoKentta(string seuraavakentta)
     {
+        // Painovoima & fysiikka
         Gravity = new Vector(0, -1000);
+        
+        // Pelikentän omaisuudet.
         TileMap kentta = TileMap.FromLevelAsset(seuraavakentta);
         kentta.SetTileMethod('#', LisaaTaso);
-        kentta.SetTileMethod('*', LisääLahja);
+        kentta.SetTileMethod('*', LisaaLahja);
         kentta.SetTileMethod('@', LisaaVihollinen);
         kentta.SetTileMethod('N', LisaaPelaaja);
-        kentta.Execute(RUUDUN_KOKO, RUUDUN_KOKO);
+        kentta.Execute(RuudunKoko, RuudunKoko);
+        
+        // Pelikentän reunat
         Level.CreateBorders();
         Level.Background.CreateGradient(Color.DarkBlue, Color.SkyBlue);
         Surface alareuna = Surface.CreateBottom(Level, 30, 100, 40);
+        
+        // Kutsut
         Add(alareuna);
         LisaaNappaimet();
         LuoLaskuri();
-        Camera.Follow(pelaaja1);
+        
+        // Pätkä, joka asettaa zoomauksen pelaajahahmoon.
+        Camera.Follow(_pelaaja1);
         Camera.ZoomFactor = 1.2;
         Camera.StayInLevel = true;
         MasterVolume = 0.5;
     }
-
-
-
-     void LisaaNappaimet()
+    
+    /// <summary>
+    /// Kategoria: PELIKENTTÄ | Ohjelma, joka luo tason. Kutsuttava.
+    /// </summary>
+    /// <param name="paikka"></param>
+    /// <param name="leveys"></param>
+    /// <param name="korkeus"></param>
+    private void LisaaTaso(Vector paikka, double leveys, double korkeus)
+    {
+        PhysicsObject taso = PhysicsObject.CreateStaticObject(leveys, korkeus);
+        taso.Position = paikka;
+        taso.Color = Color.White;
+        Add(taso);
+    }
+    
+    
+    /// <summary>
+    /// Kategoria: PELIKENTTÄ | Aliohjelma, joka vastaa kentänvaihdosta. Kutsuttava.
+    /// </summary>
+    private void SeuraavaKentta()
+    {
+        ClearAll();
+        
+        switch (_kenntaNro)
+        {
+            case 1:
+                LuoKentta(KenttaTaulukko[0]);
+                break;
+            case 2:
+                LuoKentta(KenttaTaulukko[1]);
+                break;
+            case 3:
+                LuoKentta(KenttaTaulukko[2]);
+                break;
+            case 4:
+                Exit();
+                break;
+        }
+    }
+    
+    
+    /// <summary>
+    /// Kategoria: OHJAIMET | Aliohjelma, joka asettaa ohjaimet peliin. Kutsuttava.
+    /// Nuolinäppäimet: liikkuminen & hyppääminen. Välilyönti: nyrkkeileminen
+    /// </summary>
+    private void LisaaNappaimet()
     {
 
-        Keyboard.Listen(Key.Space, ButtonState.Pressed, LisaaNyrkki, "555", pelaaja1, -NOPEUS);
-        Keyboard.Listen(Key.Left, ButtonState.Down, Liikuta, "Liikkuu vasemmalle", pelaaja1, -NOPEUS);
-        Keyboard.Listen(Key.Right, ButtonState.Down, Liikuta, "Liikkuu vasemmalle", pelaaja1, NOPEUS);
-        Keyboard.Listen(Key.Up, ButtonState.Pressed, Hyppaa, "Pelaaja hyppää", pelaaja1, HYPPYNOPEUS);
+        Keyboard.Listen(Key.Space, ButtonState.Pressed, LisaaNyrkki, "555", _pelaaja1, -Nopeus);
+        Keyboard.Listen(Key.Left, ButtonState.Down, Liikuta, "Liikkuu vasemmalle", _pelaaja1, -Nopeus);
+        Keyboard.Listen(Key.Right, ButtonState.Down, Liikuta, "Liikkuu vasemmalle", _pelaaja1, Nopeus);
+        Keyboard.Listen(Key.Up, ButtonState.Pressed, Hyppaa, "Pelaaja hyppää", _pelaaja1, HyppyNopeus);
         
         
         Keyboard.Listen(Key.F1, ButtonState.Pressed, ShowControlHelp, "Näytä ohjeet");
@@ -78,174 +140,225 @@ public class ohj1ht_peli : PhysicsGame
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
     }
 
-    void Liikuta(PlatformCharacter hahmo, double nopeus)
+    
+    /// <summary>
+    /// Kategoria: OHJAIMET | Aliohjelma, joka käsittelee liikkumisen toiminnallisuutta. Kutsuttava.
+    /// </summary>
+    /// <param name="hahmo"></param>
+    /// <param name="nopeus"></param>
+    private void Liikuta(PlatformCharacter hahmo, double nopeus)
     {
         hahmo.Walk(nopeus);
     }
 
-    void Hyppaa(PlatformCharacter hahmo, double nopeus)
+    
+    /// <summary>
+    /// Kategoria: OHJAIMET | Aliohjelma, joka käsittelee hyppyjen toiminnallisuutta. Kutsuttava.
+    /// </summary>
+    /// <param name="hahmo"></param>
+    /// <param name="nopeus"></param>
+    private void Hyppaa(PlatformCharacter hahmo, double nopeus)
     {
         hahmo.Jump(nopeus);
     }
     
-    void LisaaNyrkki(PlatformCharacter hahmo, double nopeus)
-    {
-        nyrkki = new PlatformCharacter(60, 60);
-        nyrkki.X = hahmo.X + 20;
-        nyrkki.Y = hahmo.Y;
-        nyrkki.Image = pelaajanKuva;
-        nyrkki.Tag = "nyrkki";
-        nyrkki.LifetimeLeft = TimeSpan.FromSeconds( 0.05 );
-        Add(nyrkki);
-    }
-
     
-    void LisaaTaso(Vector paikka, double leveys, double korkeus)
-    {
-        PhysicsObject taso = PhysicsObject.CreateStaticObject(leveys, korkeus);
-        taso.Position = paikka;
-        taso.Color = Color.White;
-        Add(taso);
-    }
     /// <summary>
-    /// Aliohjelma, joka luo pelaajahahmon
+    /// Kategoria: OHJAIMET | Aliohjelma, joka luo nyrkkiolion, jolla on lyhyt elinikä. Kutsuttava.
+    /// </summary>
+    /// <param name="hahmo"></param>
+    /// <param name="nopeus"></param>
+    private void LisaaNyrkki(PlatformCharacter hahmo, double nopeus)
+    {
+        _nyrkki = new PlatformCharacter(60, 60);
+        _nyrkki.X = hahmo.X + 20;
+        _nyrkki.Y = hahmo.Y;
+        _nyrkki.Image = _kuvaTaulukko[1];
+        _nyrkki.Tag = "nyrkki";
+        _nyrkki.LifetimeLeft = TimeSpan.FromSeconds( 0.05 );
+        Add(_nyrkki);
+    }
+    
+    /// <summary>
+    /// Kategoria: OHJAIMET | Aliohjelma, joak käsittelee nyrkkitörmäystapausta.
+    /// Mitä tapahtuu: kasvattaa pistemäärän, räjähdys, vihollinen kuolee.
+    /// </summary>
+    /// <param name="vihollinen"></param>
+    /// <param name="nyrkki"></param>
+    private void NyrkkiTörmäys(PhysicsObject vihollinen, PhysicsObject nyrkki)
+    {
+        Explosion rajahdys = new Explosion(50);
+        rajahdys.Position = vihollinen.Position;
+        _pelaajanPisteet.Value += 1;
+        Add(rajahdys);
+        vihollinen.Destroy();
+    }
+    
+    
+    /// <summary>
+    /// Kategoria: PELAAJA | Aliohjelma, joka luo pelaajahahmon.
+    /// Samalla luo törmäysehdot
     /// </summary>
     /// <param name="paikka"></param>
     /// <param name="leveys"></param>
     /// <param name="korkeus"></param>
-    void LisaaPelaaja(Vector paikka, double leveys, double korkeus)
+    private void LisaaPelaaja(Vector paikka, double leveys, double korkeus)
     {
-        pelaaja1 = new PlatformCharacter(leveys, korkeus);
-        pelaaja1.Position = paikka;
-        pelaaja1.Mass = 4.0;
-        pelaaja1.Image = pelaajanKuva;
-        AddCollisionHandler(pelaaja1, "Lahja", LahjaTörmäys);
-        AddCollisionHandler(pelaaja1, "vihollinen", VihollinenTörmäys);
-        Add(pelaaja1);
+        _pelaaja1 = new PlatformCharacter(leveys, korkeus);
+        _pelaaja1.Position = paikka;
+        _pelaaja1.Mass = 4.0;
+        _pelaaja1.Image = _kuvaTaulukko[1];
+        
+        // Törmäysehdot: vihollinen & lahja
+        AddCollisionHandler(_pelaaja1, "Lahja", LahjaTörmäys);
+        AddCollisionHandler(_pelaaja1, "vihollinen", VihollinenTörmäys);
+        
+        
+        Add(_pelaaja1);
     }
     
     
-    void VihollinenTörmäys(PhysicsObject pelaaja, PhysicsObject vihollinen)
+    /// <summary>
+    /// Kategoria: VIHOLLINEN | Aliohjelma, joka vastaa vihollisen törmäystapausta seuraavat tapahtumat.
+    /// Mitä tapahtuu: räjähdys, pelaajahahmo kuolee, poistutaan pelistä.
+    /// </summary>
+    /// <param name="pelaaja"></param>
+    /// <param name="vihollinen"></param>
+    private void VihollinenTörmäys(PhysicsObject pelaaja, PhysicsObject vihollinen)
     {
         Explosion rajahdys = new Explosion(50);
         rajahdys.Position = pelaaja.Position;
         Add(rajahdys);
         pelaaja.Destroy();
-        
+        Exit();
         
     }
+    
     
     /// <summary>
-    /// Aliohjelma, joak käsittelee nyrkkitörmäystapausta
-    /// </summary>
-    /// <param name="vihollinen"></param>
-    /// <param name="nyrkki"></param>
-    void NyrkkiTörmäys(PhysicsObject vihollinen, PhysicsObject nyrkki)
-    {
-        Explosion rajahdys = new Explosion(50);
-        rajahdys.Position = vihollinen.Position;
-        Add(rajahdys);
-        vihollinen.Destroy();
-        pelaajanPisteet.Value += 1;
-        
-    }
-    
-    void LisaaVihollinen(Vector paikka, double leveys, double korkeus)
-    {
-        vihollinen = new PlatformCharacter(leveys, korkeus);
-        vihollinen.Position = paikka;
-        vihollinen.Mass = 4.0;
-        vihollinen.Image = VihollinenKuva;
-        vihollinen.Tag = "vihollinen";
-        AddCollisionHandler(vihollinen, "nyrkki", NyrkkiTörmäys);
-        Add(vihollinen);
-    }
-
-    
-    /// LAHJAT
-    /// <summary>
-    /// Aliohjelma, joka luo lahjat kenttään
+    /// Kategoria: VIHOLLINEN | Aliohjelma, joka luo vihollisen kenttään.
+    /// Samalla asettaa viholliselle törmäysehdon.
     /// </summary>
     /// <param name="paikka"></param>
     /// <param name="leveys"></param>
     /// <param name="korkeus"></param>
-    void LisääLahja(Vector paikka, double leveys, double korkeus)
+    private void LisaaVihollinen(Vector paikka, double leveys, double korkeus)
     {
-        PhysicsObject Lahja = PhysicsObject.CreateStaticObject(leveys, korkeus);
-        Lahja.IgnoresCollisionResponse = true;
-        Lahja.Position = paikka;
-        Lahja.Image = tahtiKuva;
-        Lahja.Tag = "Lahja";
-        Add(Lahja);
+        _vihollinen = new PlatformCharacter(leveys, korkeus);
+        _vihollinen.Position = paikka;
+        _vihollinen.Mass = 4.0;
+        _vihollinen.Image = _kuvaTaulukko[0];
+        _vihollinen.Tag = "vihollinen";
+        AddCollisionHandler(_vihollinen, "nyrkki", NyrkkiTörmäys);
+        Add(_vihollinen);
+    }
+
+    
+    /// <summary>
+    /// Kategoria: LAHJAT | Aliohjelma, joka luo lahjat kenttään.
+    /// </summary>
+    /// <param name="paikka"></param>
+    /// <param name="leveys"></param>
+    /// <param name="korkeus"></param>
+    private void LisaaLahja(Vector paikka, double leveys, double korkeus)
+    {
+        PhysicsObject lahja = PhysicsObject.CreateStaticObject(leveys, korkeus);
+        lahja.IgnoresCollisionResponse = true;
+        lahja.Position = paikka;
+        lahja.Image = _kuvaTaulukko[2];
+        lahja.Tag = "Lahja";
+        Add(lahja);
     }
         
-    /// LAHJAT
+    
     /// <summary>
-    /// Aliohjelma, joka määrittä sen mitä tapahtuu törmäystapauksessa. Samalla lisää pisteen 
+    /// Kategoria: LAHJAT | Aliohjelma, joka vastaa lahjan törmäystapausta seuraavat tapahtumat.
+    /// Mitä tapahtuu: kasvattaa pistemäärän, lahja tuhoutuu. 
     /// </summary>
     /// <param name="hahmo"></param>
     /// <param name="tahti"></param>
-    void LahjaTörmäys(PhysicsObject hahmo, PhysicsObject tahti)
+    private void LahjaTörmäys(PhysicsObject hahmo, PhysicsObject tahti)
     {
         MessageDisplay.Add("Sait Lahjan!");
+        _pelaajanPisteet.Value += 1;
         tahti.Destroy();
-        pelaajanPisteet.Value += 1;
+        
     }
     
-    void RajaYlitetty()
+    
+    /// <summary>
+    /// Kategoria: PISTEET | Aliohjelma, joka vastaa pisteiden ylityksiä seuraavia tapahtumia.
+    /// Myös kutsuu kentänvaihto-funktiota. 
+    /// </summary>
+    private void RajaYlitetty()
     {
-        switch (pelaajanPisteet.Value)
+        switch (_pelaajanPisteet.Value)
         {
-            case pisteraja1:
+            case PisteRaja1:
+                
                 MessageDisplay.Add("Killing spree!");
                 break;
-            case pisteraja2:
+            
+            case PisteRaja2:
+                
                 MessageDisplay.Add("Rampage!");
                 break;
-            case pisteraja3:
+            
+            case PisteRaja3:
+                
                 MessageDisplay.Add("Dominating!");
                 break;
-            case pisteraja4:
+            
+            case PisteRaja4:
+                
                 MessageDisplay.Add("Unstoppable!");
                 break;
-            case pisteraja5:
+            
+            case PisteRaja5:
+                
                 MessageDisplay.Add("Godlike!");
                 break;
-            case pisteraja6:
+            
+            // Lähettää viestin, kasvattaa muuttujan yhdellä ja kutsuu aliohjelma, joka vaihtaa kenttää.
+            case PisteRaja6:
+                
                 MessageDisplay.Add("Wicked Sick!!");
-                kenttaNro++;
+                _kenntaNro++;
                 SeuraavaKentta();
                 break;
         }
     }
     
-    /// PISTEET
+    
     /// <summary>
-    /// Aliohjelma, joka lisää pistelaskurin
+    /// Kategoria: PISTEET | Aliohjelma, joka lisää pistelaskurin.
     /// </summary>
-    void LuoLaskuri()
+    private void LuoLaskuri()
     {
-        pelaajanPisteet = LuoPisteLaskuri(Screen.Right - 100.0, Screen.Top - 100.0);
-        pelaajanPisteet.Value = 0;
-        pelaajanPisteet.MaxValue = 30;
-        pelaajanPisteet.MinValue = 0;
-        pelaajanPisteet.AddTrigger(pisteraja1, TriggerDirection.Up, RajaYlitetty);
-        pelaajanPisteet.AddTrigger(pisteraja2, TriggerDirection.Up, RajaYlitetty);
-        pelaajanPisteet.AddTrigger(pisteraja3, TriggerDirection.Up, RajaYlitetty);
-        pelaajanPisteet.AddTrigger(pisteraja4, TriggerDirection.Up, RajaYlitetty);
-        pelaajanPisteet.AddTrigger(pisteraja5, TriggerDirection.Up, RajaYlitetty);
-        pelaajanPisteet.AddTrigger(pisteraja6, TriggerDirection.Up, RajaYlitetty);
+        _pelaajanPisteet = LuoPisteLaskuri(Screen.Right - 100.0, Screen.Top - 100.0);
+        _pelaajanPisteet.Value = 0;
+        _pelaajanPisteet.MaxValue = 30;
+        _pelaajanPisteet.MinValue = 0;
+        _pelaajanPisteet.AddTrigger(PisteRaja1, TriggerDirection.Up, RajaYlitetty);
+        _pelaajanPisteet.AddTrigger(PisteRaja2, TriggerDirection.Up, RajaYlitetty);
+        _pelaajanPisteet.AddTrigger(PisteRaja3, TriggerDirection.Up, RajaYlitetty);
+        _pelaajanPisteet.AddTrigger(PisteRaja4, TriggerDirection.Up, RajaYlitetty);
+        _pelaajanPisteet.AddTrigger(PisteRaja5, TriggerDirection.Up, RajaYlitetty);
+        _pelaajanPisteet.AddTrigger(PisteRaja6, TriggerDirection.Up, RajaYlitetty);
     }
     
-    /// PISTEET
+    
     /// <summary>
-    /// Aliohjelma, joka luo pistelaskurin
+    /// Kategoria: PISTEET | Aliohjelma, joka luo pistelaskurin.
     /// </summary>
-    IntMeter LuoPisteLaskuri(double x, double y)
+    private IntMeter LuoPisteLaskuri(double x, double y)
     {
+        
+        // Laskuri-olio & toiminallisuus.
         IntMeter laskuri = new IntMeter(0);
         laskuri.MaxValue = 100;
 
+        // Laskurin grafiikka.
         Label naytto = new Label();
         naytto.BindTo(laskuri);
         naytto.X = x;
@@ -255,29 +368,11 @@ public class ohj1ht_peli : PhysicsGame
         naytto.Color = Color.Green;
         naytto.Title = "Pisteet: ";
         Add(naytto);
+        
         return laskuri;
     }
     
-    void SeuraavaKentta()
-    {
-        ClearAll();
-        
-        switch (kenttaNro)
-        {
-            case 1:
-                LuoKentta("kentta1.txt");
-                break;
-            case 2:
-                LuoKentta("kentta2.txt");
-                break;
-            case 3:
-                LuoKentta("kentta3.txt");
-                break;
-            case 4:
-                Exit();
-                break;
-        }
-    }
+    
 }
    
 //ylhäältä while loop 
